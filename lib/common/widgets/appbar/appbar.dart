1f7f8b686871e5e811d8a6e9d6e3854a5e1cf92d
AdamFakee <buidinhtuan04@gmail.com>
2025-07-25 17:19:37 +0700

    feature: add darkmode to back-button in appBar header

--- lib/common/widgets/appbar/appbar.dart
+++ lib/common/widgets/appbar/appbar.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/device/device_utility.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class TAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const TAppBar({super.key, this.title, this.showBackButton = false, this.leadingIcon, this.actions, this.leadingOnPressed});
+  const TAppBar({
+    super.key,
+    this.title,
+    this.showBackButton = false,
+    this.leadingIcon,
+    this.actions,
+    this.leadingOnPressed,
+  });
 
   final Widget? title;
   final bool showBackButton;
@@ -15,19 +24,28 @@ class TAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bool isDarkMode = THelperFunctions.isDarkMode(context);
     return Padding(
       padding: EdgeInsetsGeometry.symmetric(horizontal: TSizes.md),
       child: AppBar(
         automaticallyImplyLeading: false,
         title: title,
-        leading: showBackButton ? IconButton(onPressed: () {
-          Get.back();
-        }, icon: Icon(Iconsax.arrow_left)) : leadingIcon != null ? IconButton(onPressed: leadingOnPressed, icon: Icon(leadingIcon)) : null,
+        leading: showBackButton
+            ? IconButton(
+                onPressed: () {
+                  Get.back();
+                },
+                icon: Icon(Iconsax.arrow_left),
+                color: isDarkMode ? TColors.light : TColors.dark,
+              )
+            : leadingIcon != null
+            ? IconButton(onPressed: leadingOnPressed, icon: Icon(leadingIcon))
+            : null,
         actions: actions,
       ),
     );
   }
-  
+
   @override
   Size get preferredSize => Size.fromHeight(TDeviceUtils.getAppBarHeight());
 }
