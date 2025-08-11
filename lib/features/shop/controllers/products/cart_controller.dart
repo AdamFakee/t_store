5b0c1368c492175a5391d9de6f1e024da9771d72
AdamFakee <buidinhtuan04@gmail.com>
2025-08-11 09:04:59 +0700

    features: save increa item in cart to local storage with debounce

--- lib/features/shop/controllers/products/cart_controller.dart
+++ lib/features/shop/controllers/products/cart_controller.dart
@@ -1,8 +1,10 @@
+import 'package:easy_debounce/easy_debounce.dart';
 import 'package:get/get.dart';
 import 'package:t_store/features/shop/controllers/products/variation_controller.dart';
 import 'package:t_store/features/shop/models/cart_item_model.dart';
 import 'package:t_store/features/shop/models/product_model.dart';
 import 'package:t_store/utils/constants/get_storage_key.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
 import 'package:t_store/utils/local_storage/storage_utility.dart';
 import 'package:t_store/utils/popups/confirm_popup.dart';
 import 'package:t_store/utils/popups/snack_bar.dart';
@@ -27,28 +29,26 @@ class CartController extends GetxController {
     _loadCartItems();
   }
 
-  /// load cartItems in device storage 
-  void _loadCartItems () async {
+  /// load cartItems in device storage
+  void _loadCartItems() async {
     final raws = await _storage.readData(_STORAGE_KEY);
     final List<CartItemModel> datas = raws != null
-    ? (raws as List).map((e) => CartItemModel.fromJson(e)).toList()
-    : [];
-
-
+        ? (raws as List).map((e) => CartItemModel.fromJson(e)).toList()
+        : [];
 
     cartItems.assignAll(datas);
   }
 
   /// change num of product add to cart
-  /// 
+  ///
   /// isPlus == true : a = a + 1
-  /// 
+  ///
   /// isPlus == false: a = a - 1
-  void changeNumOfProductAddToCart ({bool isPlus = true}) {
-    if(isPlus) {
+  void changeNumOfProductAddToCart({bool isPlus = true}) {
+    if (isPlus) {
       numOfProductAddToCart.value++;
     } else {
-      if(numOfProductAddToCart.value == 0) {
+      if (numOfProductAddToCart.value == 0) {
         return;
       }
 
@@ -57,10 +57,9 @@ class CartController extends GetxController {
   }
 
   /// add item to carts
-  /// 
+  ///
   /// [product] is variable pass to [addItemToCart] when user want to add item but user not navigate to detail screen. Or have not variation....
   void addItemToCart(ProductModel product) {
-
     // variables
     final variantId = _variationController.selectedVariation.value.id;
     final productId = product.id;
@@ -71,15 +70,17 @@ class CartController extends GetxController {
 
     // --check item already exits in cart
     final bool itemExists = checkItemAlreadyExitsInCart(productId, variantId);
-    if(itemExists) {
+    if (itemExists) {
       TSnackBar.customSnackBar(message: "Product already in your cart");
       return;
     }
     // check stock
 
     final bool outOfStock = checkItemOutOfStock(product, variantId);
-    if(outOfStock) {
-      final message = variantId.isNotEmpty ? "Variant is out of stock" : "Product is out of stock";
+    if (outOfStock) {
+      final message = variantId.isNotEmpty
+          ? "Variant is out of stock"
+          : "Product is out of stock";
       TSnackBar.customSnackBar(message: message);
       return;
     }
@@ -93,15 +94,19 @@ class CartController extends GetxController {
     // reset attribute of variation
     _variationController.resetSelectedAttributes();
 
-    // reset 
+    // reset
     numOfProductAddToCart.value = 0;
     cartItems.refresh();
   }
 
-  bool checkItemAlreadyExitsInCart (String productId, [String variationId = ""]) {
-    if(variationId.isNotEmpty) {
+  bool checkItemAlreadyExitsInCart(
+    String productId, [
+    String variationId = "",
+  ]) {
+    if (variationId.isNotEmpty) {
       return cartItems.any((cartItem) {
-        return cartItem.productId == productId && cartItem.variationId == variationId;
+        return cartItem.productId == productId &&
+            cartItem.variationId == variationId;
       });
     } else {
       return cartItems.any((cartItem) {
@@ -110,37 +115,53 @@ class CartController extends GetxController {
     }
   }
 
-  bool checkItemOutOfStock (ProductModel product, [String variationId = ""]) {
-    if(variationId.isNotEmpty) {
+  bool checkItemOutOfStock(ProductModel product, [String variationId = ""]) {
+    if (variationId.isNotEmpty) {
       return _variationController.selectedVariation.value.stock <= 0;
     } else {
       return product.stock <= 0;
     }
   }
 
-  CartItemModel convertProductToCartItem (ProductModel product) {
+  CartItemModel convertProductToCartItem(ProductModel product) {
     // variables
     final variant = _variationController.selectedVariation.value;
     final productId = product.id;
 
     return CartItemModel(
-      productId: productId, 
+      productId: productId,
       quantity: numOfProductAddToCart.value,
       title: product.title,
       variationId: variant.id,
       price: variant.id != "" ? variant.salePrice : product.salePrice,
       image: variant.id != "" ? variant.image : product.thumbnail,
       brandName: product.brand?.name ?? "",
-      selectedVariation: Map<String, String>.from(_variationController.selectedAttributes)
+      selectedVariation: Map<String, String>.from(
+        _variationController.selectedAttributes,
+      ),
     );
   }
 
-  void save() {
+  /// function used to save [cartItems] in localstorage
+  ///
+  /// [isAddItem] is a flag to check whether this action that call [save] is "Add item to cart" or not.
+  ///
+  /// [isAddItem = true] => show alter
+  void save({bool isAddItem = true}) {
     try {
       _storage.saveData(_STORAGE_KEY, cartItems);
-      TSnackBar.customSnackBar(message: "Product added to your cart");
+      if (isAddItem) {
+        TSnackBar.customSnackBar(message: "Product added to your cart");
+      }
     } catch (_) {
-      TSnackBar.customSnackBar(message: "Can't add product to your cart");
+      if (isAddItem) {
+        TSnackBar.customSnackBar(message: "Can't add product to your cart");
+      } else {
+        TSnackBar.errorSnackBar(
+          title: "Oh",
+          message: TTexts.somethingWentWrong,
+        );
+      }
     }
   }
 
@@ -153,13 +174,13 @@ class CartController extends GetxController {
     return 0;
   }
 
-  /// increa number of specific product in cart. if num = 0 => show popup remove 
-  /// 
+  /// increa number of specific product in cart. if num = 0 => show popup remove
+  ///
   /// [isPlus] = true => +1
-  /// 
+  ///
   /// [isPlus] = false => -1
-  /// 
-  /// 
+  ///
+  ///
   /// using [cartItemIndex] to up perfomance when user spamly change [cartItem.quantity]. If have [cartItemIndex] => we dont need to loop all [cartItems].
   void changeProductQuantityInCart({
     required String productId,
@@ -174,19 +195,28 @@ class CartController extends GetxController {
       cartItems[cartItemIndex] = currentItem.copyWith(quantity: currentQty + 1);
     } else {
       if (currentQty > 1) {
-        cartItems[cartItemIndex] = currentItem.copyWith(quantity: currentQty - 1);
+        cartItems[cartItemIndex] = currentItem.copyWith(
+          quantity: currentQty - 1,
+        );
       } else {
         // if [quatity] = 1 && [isPlus] = false => show popup ask user: "this action will remove this item".
-        final bool confirm = await TConfirmPopup.show(title: "This item in cart will be remove. Are you sure?");
+        final bool confirm = await TConfirmPopup.show(
+          title: "This item in cart will be remove. Are you sure?",
+        );
 
-        if(confirm) {
+        if (confirm) {
           cartItems.removeAt(cartItemIndex);
         }
-        return;
       }
     }
 
     cartItems.refresh(); // Cập nhật UI
-  }
 
+    // save to local storage
+    EasyDebounce.debounce(
+      "ADD_ITEM_TO_CART",
+      Duration(seconds: 1),
+      () => save(isAddItem: false),
+    );
+  }
 }
